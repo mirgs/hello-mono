9760160fcbceabe1b08feb9b38dda0f9a967d8d0
Miriam <mgarcias@viewnext.com>
2021-01-27 12:44:32 +0100

    Archivos del proyecto Mono

---
+++ Mono-MyFirstNancy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace MonoMyFirstNancy
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			using (var host = new NancyHost(new Uri("http://0.0.0.0:4321")))
+			{
+				host.Start();
+				Console.WriteLine("Running on http://0.0.0.0:4321");
+				Console.ReadLine();
+			}
+        }
+    }
+
+	public class SampleModule : Nancy.NancyModule
+	{
+		public SampleModule()
+		{
+			Get["/"] = _ => "Hello World!";
+		}
+	}
+}
